2a8349e3cdc9334d347e6de1ad154b81d588b90d
Denis <denisganev@gmail.com>
2015-03-05 01:44:00 -0500

    all

---
+++ .env.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$_ENV['DB_NAME'] = 'pixelintel';
+$_ENV['DB_USER'] = 'root';
+$_ENV['DB_PASSWORD'] = 'root';
+$_ENV['DB_HOST'] = '127.0.0.1';
+$_ENV['AWS_ACCESS_KEY_ID'] = '';
+$_ENV['AWS_SECRET_KEY'] = '';
+$_ENV['MEMCACHED_NODE_ONE'] = '127.0.0.1:11211';
+$_ENV['DISABLE_MEMCACHE'] = 1;
+
