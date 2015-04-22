cd7491cfb7c135b2dff68db9756e7bc8b94617a3
denis <denisganev@gmail.com>
2015-04-22 12:50:24 -0400

    all

--- wp-content/themes/genie/singledesktop.php
+++ wp-content/themes/genie/singledesktop.php
@@ -338,6 +338,21 @@ if ( have_posts() ) {
             <i class="fa fa-facebook-official"></i> Share on Facebook
         </a>
     </div>
+      <div style="text-align: center;">
+        <div style="display: inline-block;">
+            <div style="font-size: 12px;">Advertisement</div>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- 336x280 -->
+	<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-4049798989734696"
+     data-ad-slot="6912161568"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
+
     <div class="contentID" style="text-align:center;">
     <div id="contentad49382"></div>
 <script type="text/javascript">
--- wp-content/themes/genie/singlemobile.php
+++ wp-content/themes/genie/singlemobile.php
@@ -333,6 +333,21 @@ if ( have_posts() ) {
         </a>
     </div>
 
+          <div style="text-align: center;">
+        <div style="display: inline-block;">
+            <div style="font-size: 12px;">Advertisement</div>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- 336x280 -->
+	<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-4049798989734696"
+     data-ad-slot="6912161568"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
+
 
 <?php
 
