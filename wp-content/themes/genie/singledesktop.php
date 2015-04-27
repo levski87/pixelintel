ac54ec283525efbe7bb7288785a9b3d15bb6ea83
denis <denisganev@gmail.com>
2015-04-26 22:19:01 -0400

    all

--- wp-content/themes/genie/singledesktop.php
+++ wp-content/themes/genie/singledesktop.php
@@ -341,7 +341,7 @@ if ( have_posts() ) {
 
 
 <?php $postid = get_the_ID(); 
-	if ($postid != 871 ): ?>
+	if ($postid != 871 && $postid != 697 ): ?>
 		<!-- 336x280 -->
 	 <div style="text-align: center;">
         <div style="display: inline-block;">
